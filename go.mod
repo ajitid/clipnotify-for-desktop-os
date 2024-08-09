02618b47421d374a80f31b6b86dc4edac4940496
Ajit <zlksnkwork@gmail.com>
2024-08-10 00:59:55 +0530

    create a go based clipboard monitor for windows

---
+++ go.mod
@@ -0,0 +1,5 @@
+module clipnotify
+
+go 1.22.5
+
+require github.com/gonutz/w32/v2 v2.11.1
---
+++ main.go
@@ -0,0 +1,68 @@
+// original sources: https://abdus.dev/projects/dumpclip/ and https://abdus.dev/posts/monitor-clipboard/
+package main
+
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+
+	"github.com/gonutz/w32/v2"
+)
+
+const (
+	WM_CLIPBOARDUPDATE = 0x031D
+)
+
+func main() {
+	// Create a window to receive messages
+	className, _ := syscall.UTF16PtrFromString("ClipboardMonitorClass")
+	wc := w32.WNDCLASSEX{
+		WndProc:   syscall.NewCallback(wndProc),
+		ClassName: className,
+	}
+	w32.RegisterClassEx(&wc)
+
+	hwnd := w32.CreateWindowEx(
+		0,
+		className,
+		nil,
+		0,
+		0, 0, 0, 0,
+		0,
+		0,
+		0,
+		nil,
+	)
+
+	// Add the window as a clipboard viewer
+	w32.AddClipboardFormatListener(hwnd)
+
+	fmt.Println("Monitoring clipboard for changes. Press Ctrl+C to exit.")
+
+	// Message loop
+	var msg w32.MSG
+	for w32.GetMessage(&msg, 0, 0, 0) != 0 {
+		w32.TranslateMessage(&msg)
+		w32.DispatchMessage(&msg)
+	}
+}
+
+func wndProc(hwnd w32.HWND, msg uint32, wParam, lParam uintptr) uintptr {
+	switch msg {
+	case WM_CLIPBOARDUPDATE:
+		if w32.OpenClipboard(hwnd) {
+			defer w32.CloseClipboard()
+
+			hClipData := w32.GetClipboardData(w32.CF_UNICODETEXT)
+			if hClipData != 0 {
+				pszText := w32.GlobalLock(w32.HGLOBAL(hClipData))
+				if pszText != nil {
+					defer w32.GlobalUnlock(w32.HGLOBAL(hClipData))
+					clipboardText := w32.UTF16PtrToString((*uint16)(unsafe.Pointer(pszText)))
+					fmt.Printf("New clipboard content: %s\n", clipboardText)
+				}
+			}
+		}
+	}
+	return w32.DefWindowProc(hwnd, msg, wParam, lParam)
+}
